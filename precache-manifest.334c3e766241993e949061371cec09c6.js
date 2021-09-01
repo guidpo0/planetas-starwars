c1d28f79fa1cc6d977fade4a1de745c447c80995
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-01 01:14:10 -0300

    Updates

---
+++ precache-manifest.334c3e766241993e949061371cec09c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1647277c2b32a2590091595ae76f2037",
+    "url": "/planetas-starwars/index.html"
+  },
+  {
+    "revision": "a0d88fdd15537a5ca68a",
+    "url": "/planetas-starwars/static/css/main.cfff8c7a.chunk.css"
+  },
+  {
+    "revision": "0ef555c027da8a73f50e",
+    "url": "/planetas-starwars/static/js/2.5148df56.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/planetas-starwars/static/js/2.5148df56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d88fdd15537a5ca68a",
+    "url": "/planetas-starwars/static/js/main.1cb2f3a0.chunk.js"
+  },
+  {
+    "revision": "559bafc7aa1e7ba4292a",
+    "url": "/planetas-starwars/static/js/runtime-main.0d0fe89a.js"
+  },
+  {
+    "revision": "db1a6fdcbd791d5460daedac90b42590",
+    "url": "/planetas-starwars/static/media/bg.db1a6fdc.jpg"
+  },
+  {
+    "revision": "a41cce90b841dd861d5712ab6a9ca617",
+    "url": "/planetas-starwars/static/media/starwars-logo.a41cce90.png"
+  }
+]);
